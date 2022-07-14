ae3352658ee9e85d12256b524fc14baaeb7eb9ba
choihs <choihs1998@naver.com>
2022-07-14 23:44:14 +0900

    reconstruct algorithm type

--- src/8. Queue,Deque/B#1966.py
+++ src/8. Queue,Deque/B#1966.py
@@ -1,21 +1,60 @@
-N = int(input())
+# 2022.03.10
+# 프린터 큐 - Queue 문제
+# 참고 링크: https://www.acmicpc.net/problem/1966
 
-for i in range(N):
-    n,m = map(int,input().split())
-    imp = list(map(int,input().split()))
-    pos = [False]*n
-    pos[m] = True
+Test_Case = int(input())
+
+for i in range(Test_Case):
+    N, M = map(int,input().split())
+    Queue = list(map(int,input().split()))
+    check = [True]*N
+    check[M] = False
     cnt = 1
     
-    while True:
-        if imp[0] == max(imp):
-            if pos[0]:
-                print(cnt)
-                break
-            else:
-                imp.pop(0)
-                pos.pop(0)
-                cnt += 1
+    while check[0] or max(Queue) != Queue[0]:
+        MAX = max(Queue)
+        if MAX != Queue[0]:
+            Queue.append(Queue[0])
+            Queue.pop(0)
+            check.append(check[0])
+            check.pop(0)
         else:
-            imp.append(imp.pop(0))
-            pos.append(pos.pop(0))
+            Queue.pop(0)
+            check.pop(0)
+            cnt += 1
+
+    print(cnt)
+
+########################다른 사람 풀이##################################
+# import sys
+
+
+# t = int(sys.stdin.readline())
+
+# for _ in range(t):
+#     N, M = map(int, sys.stdin.readline().split())
+#     lst = list(map(int, sys.stdin.readline().split()))
+#     target = lst[M]
+#     cnt = 0
+#     location = M
+    
+#     while lst:
+#         high = max(lst)
+#         if lst[0] < high:
+#             if location == 0:
+#                 low = lst.pop(0)
+#                 lst.append(low)
+#                 location = len(lst) - 1
+#             else:
+#                 low = lst.pop(0)
+#                 lst.append(low)
+#                 location -= 1
+                
+#         else:
+#             done = lst.pop(0)
+#             cnt += 1
+#             if done == target and location == 0:
+#                 print(cnt)
+#                 break
+#             else:
+#                 location -= 1
--- src/etc/알고리즘 문제풀이/B#1978.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-N = int(input())
-num = list(map(int,sys.stdin.readline().split()))
-cnt = 0
