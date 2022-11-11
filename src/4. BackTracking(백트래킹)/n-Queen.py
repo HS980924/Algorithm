65f0867844ac5568ba713b97bc3dac351a941071
HS980924 <choihs980924@gmail.com>
2022-11-12 02:40:57 +0900

    n-queen solved

--- src/4. BackTracking(백트래킹)/n-Queen.py
+++ src/4. BackTracking(백트래킹)/n-Queen.py
@@ -69,9 +69,60 @@
         
 #     return True
 
+# def solution(n):
+#     answer = 0
+#     global board
+#     board = [-1]*n
+#     answer = backTracking(0, n)
+#     return answer
+
+# def checkBoard(x, y):
+#     for i in range(y):
+#         if abs(x-board[i]) == 0 or abs(x-board[i]) == abs(y-i):
+#             return False
+#     return True
+
+# def backTracking(idx, n):
+#     if idx == n:
+#         return 1
+#     else:
+#         result = 0
+#         if not idx:
+#             for value in range(n):
+#                 board[idx] = value
+#                 result += backTracking(idx+1,n)
+#         else:
+#             for value in range(n):
+#                 if checkBoard(value, idx):
+#                     board[idx] = value
+#                     result += backTracking(idx+1,n)
+                    
+#         return result
+# print(solution(4))
+
 def solution(n):
     answer = 0
-    board = [0]*(n+1)
-    
-    
+    global board
+    board = [-1]*n
+    for i in range(n):
+        board[0] = i
+        answer += nqueen(1,n)
     return answer
+
+def nqueen(x,n):
+    if x == n:
+        return 1
+    else:
+        result = 0
+        for y in range(n):
+            check = True
+            for x2 in range(x):
+                if y == board[x2] or abs(y-board[x2]) == abs(x-x2):
+                    check = False
+                    break
+            if check:
+                board[x] = y
+                result += nqueen(x+1,n)
+                
+        return result 
+print(solution(4))
